00c0fea34c2e242a858f3e11040bb2903c053a59
bendigiorgio <bendigiorgio@gmail.com>
2023-06-18 23:16:10 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# MD Components
+
+A component pack for React/Next.js (Depending on the component).
+
+This has a lot of requirements but feel free to just copy the code for each component individually if you don't want to install all the dependencies.
+
+## Installation
---
+++ src/index.ts
@@ -0,0 +1,70 @@
+import { AspectRatio } from "./components/aspect-ratio";
+import { Button } from "./components/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./components/card";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  useFormField,
+} from "./components/form";
+import { Input } from "./components/input";
+import { Label } from "./components/label";
+import { ScrollArea } from "./components/scroll-area";
+import { Separator } from "./components/separator";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "./components/tabs";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./components/tooltip";
+import { Loader } from "./components/loader";
+
+import { cn } from "./lib/utils";
+
+// Components
+export {
+  AspectRatio,
+  Button,
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  useFormField,
+  Input,
+  Label,
+  ScrollArea,
+  Separator,
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+  Loader,
+};
+
+// Utils
+export { cn };
