766c632ab60e41966f60555e0f8c2e95eca6e42a
bendigiorgio <bendigiorgio@gmail.com>
2023-06-24 12:44:38 +0900

    storybook ts fixes

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,18 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import path from "path";
+
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    {
+      name: "@storybook/addon-styling",
+      options: {
+        postCss: true,
+      },
+    },
   ],
   framework: {
     name: "@storybook/nextjs",
@@ -12,6 +20,19 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: "tag",
+  },
+  webpackFinal: async (config) => {
+    // @ts-expect-error
+    config.resolve.alias = {
+      "@": path.resolve(__dirname, "../src"),
+    };
+    // @ts-expect-error
+    config.resolve.extensionAlias = {
+      ".js": [".ts", ".js"],
+      ".mjs": [".mts", ".mjs"],
+    };
+
+    return config;
   },
 };
 export default config;
