a401b55e0675c55cb95f0d5af2900c60582b7e95
bendigiorgio <bendigiorgio@gmail.com>
2023-06-27 15:09:28 +0900

    feature card

---
+++ src/prebuilt/feature-card/feature-card.stories.tsx
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { FeatureCard } from "./feature-card";
+import { Bomb } from "lucide-react";
+import React from "react";
+const meta: Meta<typeof FeatureCard> = {
+  title: "Prebuilt/FeatureCard",
+  component: FeatureCard,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof FeatureCard>;
+
+export const Default: Story = {
+  args: {
+    title: "Title",
+    description: "Description",
+    icon: <Bomb className="w-full h-full" />,
+  },
+};
+
+export const WithColor: Story = {
+  args: {
+    className: "bg-muted",
+    color: "#04ddfa",
+    title: "Title",
+    description: "Description",
+    icon: <Bomb className="w-full h-full" />,
+  },
+};
---
+++ src/prebuilt/feature-card/feature-card.tsx
@@ -0,0 +1,39 @@
+import { Card, CardContent, CardHeader } from "@/components/card/index.js";
+import { cn } from "@/lib/utils.js";
+import React from "react";
+
+interface FeatureCardProps extends React.ButtonHTMLAttributes<HTMLDivElement> {
+  icon?: React.ReactNode;
+  title: string;
+  description: React.ReactNode;
+  color?: string;
+  gradient?: string[];
+}
+
+const FeatureCard = React.forwardRef<HTMLDivElement, FeatureCardProps>(
+  ({ icon, title, description, className, color, gradient, ...props }, ref) => {
+    const backgroundStyle = gradient
+      ? { backgroundImage: `linear-gradient(to right, ${gradient.join(", ")})` }
+      : { backgroundColor: color };
+    return (
+      <Card ref={ref} {...props} className={cn("w-full max-w-xs", className)}>
+        <CardHeader className="pb-0">
+          {icon && (
+            <div style={{ color: color }} className="h-10 w-10 ml-1 mb-0.5">
+              {icon}
+            </div>
+          )}
+          <h4 className="text-lg font-medium">{title}</h4>
+          <div
+            style={backgroundStyle}
+            className={`h-0.5 w-16 rounded-full bg-accent-foreground`}
+          />
+        </CardHeader>
+        <CardContent className="mt-2">{description}</CardContent>
+      </Card>
+    );
+  }
+);
+FeatureCard.displayName = "FeatureCard";
+
+export { FeatureCard };
