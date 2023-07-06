fbda34c4a68e73699d6f9dfcedc90ecb213254c9
bendigiorgio <bendigiorgio@gmail.com>
2023-07-06 11:57:03 +0900

    convert to async

--- src/lib/src/zod-utils/safeFetch.ts
+++ src/lib/src/zod-utils/safeFetch.ts
@@ -1,15 +1,13 @@
 import * as z from "zod";
 
-const zodSafeFetch = <TData>(
+const zodSafeFetch = async <TData>(
   url: string,
   schema: z.Schema<TData>
 ): Promise<TData> => {
-  return fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-      const parsedData = schema.parse(data);
-      return parsedData;
-    });
+  const res = await fetch(url);
+  const data = await res.json();
+  const parsedData = schema.parse(data);
+  return parsedData;
 };
 
 export { zodSafeFetch };
